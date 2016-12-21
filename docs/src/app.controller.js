bd3de01d3d146d877eb764ac1f746fc9abfce9ad
Joe Hawkins <jedwardhawkins@gmail.com>
2016-12-20 17:29:18 -0700

    Components tab in nav ui-sref-active works

--- docs/src/app.controller.js
+++ docs/src/app.controller.js
@@ -1,13 +1,16 @@
 angular.module('app').controller('AppController', AppController);
 
-function AppController($document, config) {
+function AppController($document, $state, config) {
     this.projectName = config.projectName;
     this.$document = $document;
+    this.$state = $state;
     this.themeLink = this.$document[0].getElementById('theme-link');
 }
 
-AppController.prototype.usingDefaultTheme = function() {
-    return this.themeLink.href.indexOf('_dark') < 0;
+AppController.prototype.goToComponentsView = function($event) {
+    $event.preventDefault();
+
+    this.$state.go('app.component.appBar');
 };
 
 AppController.prototype.toggleTheme = function() {
@@ -17,4 +20,8 @@ AppController.prototype.toggleTheme = function() {
     else {
         this.themeLink.href = this.themeLink.href.replace('_dark', '');
     }
+};
+
+AppController.prototype.usingDefaultTheme = function() {
+    return this.themeLink.href.indexOf('_dark') < 0;
 };
