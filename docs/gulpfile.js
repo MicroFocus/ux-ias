d66ec01d4a0de235c87f05eac475e193b20dfc79
Joe Hawkins <jedwardhawkins@gmail.com>
2017-04-05 16:53:34 -0600

    Upgrade ng-gulp to 0.7.2 to allow for directly linking to mfux.css and mfux_dark.css in index.html

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -2,37 +2,45 @@ var gulp = require('gulp');
 var ngGulp = require('ng-gulp');
 var path = require('path');
 
-var cwd = process.cwd();
-
 ngGulp(gulp, {
+    devServer: {
+        root: [
+            path.resolve(__dirname, '../dist'),
+            path.resolve(__dirname, '../dist/docs')
+        ]
+    },
     directories: {
-        nodeModules: path.resolve(cwd, '../node_modules'),
-        output: path.resolve(cwd, '../dist/docs'),
-        outputImages: path.resolve(cwd, '../dist/docs/images'),
-        outputVendor: path.resolve(cwd, '../dist/docs/vendor')
+        nodeModules: path.resolve(__dirname, '../node_modules'),
+        output: path.resolve(__dirname, '../dist/docs'),
+        outputImages: path.resolve(__dirname, '../dist/docs/images'),
+        outputVendor: path.resolve(__dirname, '../dist/docs/vendor')
     },
     externals: {
         'angular-ui-router': 'window["angular-ui-router"]'
     },
     files: {
-        sassManifest: [
-            'src/app.scss',
-            'src/app_dark.scss'
-        ],
+        indexProduction: 'src/index.production.html',
+        sassManifest: 'src/app.scss',
         vendorDevelopment: [
-            path.resolve(cwd, '../node_modules/angular/angular.js'),
-            path.resolve(cwd, '../node_modules/angular-ui-router/release/angular-ui-router.js'),
-            path.resolve(cwd, '../node_modules/mf-icons/dist/**/*')
+            path.resolve(__dirname, '../node_modules/angular/angular.js'),
+            path.resolve(__dirname, '../node_modules/angular-ui-router/release/angular-ui-router.js'),
+            path.resolve(__dirname, '../node_modules/mf-icons/dist/**/*')
         ],
         vendorProduction: [
-            path.resolve(cwd, '../node_modules/angular/angular.js'),
-            path.resolve(cwd, '../node_modules/angular-ui-router/release/angular-ui-router.js'),
-            path.resolve(cwd, '../node_modules/mf-icons/dist/**/*')
+            path.resolve(__dirname, '../node_modules/angular/angular.js'),
+            path.resolve(__dirname, '../node_modules/angular-ui-router/release/angular-ui-router.js'),
+            path.resolve(__dirname, '../node_modules/mf-icons/dist/**/*')
         ],
         vendorTest: [
-            path.resolve(cwd, '../node_modules/angular/angular.js'),
-            path.resolve(cwd, '../node_modules/angular-mocks/angular-mocks.js'),
-            path.resolve(cwd, '../node_modules/angular-ui-router/release/angular-ui-router.js'),
+            path.resolve(__dirname, '../node_modules/angular/angular.js'),
+            path.resolve(__dirname, '../node_modules/angular-mocks/angular-mocks.js'),
+            path.resolve(__dirname, '../node_modules/angular-ui-router/release/angular-ui-router.js')
+        ]
+    },
+    productionServer: {
+        root: [
+            path.resolve(__dirname, '../dist'),
+            path.resolve(__dirname, '../dist/docs')
         ]
     }
 });
