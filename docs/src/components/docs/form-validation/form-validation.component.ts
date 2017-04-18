efa5dc06468afaf08e8a9912ec21e6a9a033df10
Lynn Christensen <Lynn.Christensen@microfocus.com>
2017-04-18 16:13:21 -0600

    new form validation docs and adjustments to form-field styles

---
+++ docs/src/components/docs/form-validation/form-validation.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '../../../component.decorator';
+
+@Component({
+    templateUrl: require('./form-validation.component.html')
+})
+export default class FormFieldComponent {}
